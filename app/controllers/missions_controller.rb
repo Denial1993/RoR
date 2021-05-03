2b41947eb649f878f0ccbe8fea40feae7045b96d
denial <41332190+Denial1993@users.noreply.github.com>
2021-05-03 13:21:28 +0000

    第七步驟完成

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,22 +1,51 @@
 class MissionsController < ApplicationController
   def index
+    @missions = Mission.all 
   end
 
   def new
+    @mission = Mission.new
   end
 
   def create
+    @mission = Mission.new(mission_params)
+      if @mission.save
+        redirect_to missions_path,notice: "新增成功"
+      else
+        render :new
+      end
   end
 
   def show
+#    @mission = Mission.find
   end
 
   def edit
+    @mission = Mission.find_by(id: params[:id])
   end
 
   def update
+    @mission = Mission.find_by(id: params[:id])
+    #這邊有新增原本資料庫內沒有的新版資料,所以要用mission_params 過濾資訊
+    if @mission.update(mission_params)
+      redirect_to missions_path ,notice: "資料更新成功!"
+    else
+      render :edit
+    end
+    
   end
 
-  def delete
+  def destroy
+    @mission = Mission.find_by(id: params[:id])
+  
+    if @mission
+      @mission.destroy
+      redirect_to missions_path,notice: "任務刪除成功!"
+    end
+  end
+  
+  private
+  def mission_params
+    params.require(:mission).permit(:name, :description, :start, :end)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
 #  post 'missions/update'  
 #  get 'missions/delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "missions#index"
   resources :missions
 end
