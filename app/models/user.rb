59b79eef00e7c6538787d2b00b6b48f4f73d8217
denial <41332190+Denial1993@users.noreply.github.com>
2021-05-24 12:54:32 +0000

    步驟19

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
 #  確保唯一
   validates_uniqueness_of :email
+  
+  
 #  確保格式正確 
 #  參考資料網址: https://ihower.tw/rails/activerecord-lifecycle.html
   validates_format_of :email, :with => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  
+  has_many :missions
 end
